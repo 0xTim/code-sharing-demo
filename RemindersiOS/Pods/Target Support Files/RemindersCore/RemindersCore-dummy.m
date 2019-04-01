5cf20212859810ed526a601aa0843d0afe9c4da7
Tim <0xtimc@gmail.com>
2019-04-02 00:47:37 +0100

    Integrate RemindersCore with iOS Project{

---
+++ RemindersiOS/Pods/Target Support Files/RemindersCore/RemindersCore-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RemindersCore : NSObject
+@end
+@implementation PodsDummy_RemindersCore
+@end
---
+++ RemindersiOS/RemindersiOS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RemindersiOS
+//
+//  Created by Tim Condon on 02/04/2019.
+//  Copyright © 2019 Tim Condon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
