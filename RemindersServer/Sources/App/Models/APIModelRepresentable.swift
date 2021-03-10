d6e334d1c95a3062da146a2b1ad8bf35c68ccb90
Tim <0xtimc@gmail.com>
2021-03-10 22:33:37 +0000

    Update for final try

--- RemindersServer/Sources/App/Models/APIModelRepresentable.swift
+++ RemindersServer/Sources/App/Models/APIModelRepresentable.swift
@@ -1,23 +1,22 @@
-
 import Foundation
 import Vapor
 
 protocol APIModelRepresentable {
     associatedtype APIModelRepresentationType: Content
-    func apiModel() throws -> APIModelRepresentationType
+    func apiModel() -> APIModelRepresentationType
 }
 
 // MARK: Convenience Extensions
 extension EventLoopFuture where Value: APIModelRepresentable {
-    func apiModel() throws -> EventLoopFuture<Value.APIModelRepresentationType> {
-        return flatMapThrowing { try $0.apiModel() }
+    func apiModel() -> EventLoopFuture<Value.APIModelRepresentationType> {
+        return map { $0.apiModel() }
     }
 }
 
 extension Array: APIModelRepresentable where Element: APIModelRepresentable {
     typealias APIModelRepresentationType = [Element.APIModelRepresentationType]
 
-    func apiModel() throws -> [Element.APIModelRepresentationType] {
-        return try map { try $0.apiModel() }
+    func apiModel() -> [Element.APIModelRepresentationType] {
+        return map { $0.apiModel() }
     }
 }
